74902a98f5e76f9129fc462bea6760ab03353cb8
SIDHARTH T <sidhartht900@gmail.com>
2024-12-11 23:05:52 +0530

    Ass Q 4

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Accept input from the user
-let P = Number(prompt("Enter Principal amount :")); 
-let R = Number(prompt("Enter a Interest rate (float):"));
-let n = Number(prompt("Enter a Number of years (float):"));
+let mark = Number(prompt("Enter Your Mark out of 100:")); 
 
-// Display Simple Interest (SI) in Console 
-console.log("Simple Interest (SI): " +((P*R*n)/100)
-);
+// Display Pass or Failed 
+
+console.log(PorF());
+
+function PorF(){
+    if(mark < 50){
+        result="Sorry, Your Failed";
+    }else if(mark>100){
+        result="Mark out of 100!!!\nEnter a valid mark"
+    }else{
+        result="Congratulation ,Your Passed";
+    }
+  return result;
+}
