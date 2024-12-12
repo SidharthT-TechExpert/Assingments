6650cf75c1759135853d18915740264de0fa18a6
SIDHARTH T <sidhartht900@gmail.com>
2024-12-12 10:06:57 +0530

    Ass Q10

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-// Accept input from the user
-let limit = Number(prompt("Enter a limit:")); 
+// Array Size from User
 
-// Display Sum of odd Number b/w Limit
-console.log("Sum of odd numbers = "+oddSum());
+let size = Number(prompt("Enter A Array Size:"));
 
-function oddSum(){
-    let sum = 0;
-   for (let i = 1 ; i < limit ; i ++){
-       if (i % 2 == 1){
-           sum +=i;
-       }
-   }
-  if (sum == 0){
-      sum ="No Number In Odd Between this Limit";
-  }
-  
-  return sum ;
+// Checking Size is Valid or not 
+
+if (isNaN(size) || size <= 0){
+    console.log("Invalid Array Size Entered!!");
+}else{
+    console.log("Enter Array 1 values:")
+     let array1 = getArrayInput( size );
+    
+    
+    console.log("Enter Array 2 Values:")
+      let array2 = getArrayInput(size);
+      
+      swapArray(array1,array2);
+      
+    // Desplay After Swapping  
+      console.log("Arrays after swapping:");
+      console.log("Array 1: "+array1);
+      console.log("Array 2: "+array2);
+}
+// get Array elements
+function getArrayInput(arraysize){
+    let array = [];
+    
+    for(i = 0 ;i < arraysize ;i++){
+      let value = Number(prompt(`Value ${i+1} :`));
+      array.push(value);
+    }
+    return array;
+}
+
+// Swaping of Array
+function swapArray(arr1 , arr2){
+    let temp = 0 ; 
+    
+    for(let i = 0 ;i < size ;i++){
+        temp = arr1[i];
+        arr1[i] = arr2[i];
+        arr2[i] = temp;
+    }
 }
