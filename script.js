01580c6fbaea6c98dddaaed933bef872b8b28c90
SIDHARTH T <sidhartht900@gmail.com>
2024-12-12 07:39:33 +0530

    Ass Q 5

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Accept input from the user
-let mark = Number(prompt("Enter Your Mark out of 100:")); 
-
+let totalMark = Number(prompt("Enter Your Total Mark Percentage Out Of 100 (Float):")); 
 // Display Pass or Failed 
+console.log(Grade());
 
-console.log(PorF());
-
-function PorF(){
-    if(mark < 50){
-        result="Sorry, Your Failed";
-    }else if(mark>100){
-        result="Mark out of 100!!!\nEnter a valid mark"
+function Grade(){
+    if(totalMark > 100){
+        result = "Total Mark Percentage Out Of 100!!!\nEnter A Valid Total mark Pesentage";
+    }else if(totalMark > 90){
+        result = "Congratulation ,\n Your Grade Is 'A'";
+    }else if(totalMark > 80){
+        result = "Congratulation ,\n Your Grade Is 'B'";
+    }else if(totalMark > 70){
+        result = "Your Grade Is 'C'";
+    }else if(totalMark > 60){
+        result = "Your Grade Is 'D'";
+    }else if (totalMark > 50){
+        result = "Your Grade Is 'E'";
     }else{
-        result="Congratulation ,Your Passed";
+        result="Sorry, Your Failed !!!";
     }
   return result;
 }
