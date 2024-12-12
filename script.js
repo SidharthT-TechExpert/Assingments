27ce9055710a87e6d1ca751c0ba263d1b8a89b7f
SIDHARTH T <sidhartht900@gmail.com>
2024-12-12 22:25:07 +0530

    Ass Q 14 MultiDiem

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-//Getting Value
-let num = Number(prompt("Enter a Number:"));
-//clecking entered value is prime or not
-let result = primeOrNot();
-//Printing OutPuts
-console.log(result);
+//Getting size of Array
+let size = Number(prompt("Enter a arry size:"));
 
-// Function Of Prime or Not
-function primeOrNot(){
-     if(num < 2){
-          return "Enterd Number is not a prime number";
-        }
-    for(i = 2;i <= num/2;i++){
-         if(num % i == 0){
-           return "Enterd Number is not a prime number";
-          }
-      }
+//Getting Array1 Value 
+console.log('Enter the '+(size*size)+' values to array 1')
+ let array1 = getArray();
+ 
+//Getting Array2 Value  
+console.log('Enter the '+(size*size)+' values to array 2')
+ let array2 = getArray();
 
-  return "Enterd Number is a prime number";
+//Adding Two Array
+let array = addingArrays();
+
+//Printing Result
+console.log('The sum of two given arrays : ')
+ DisplayArray()
+
+// Fun Getting Array Values
+function getArray(){
+    let array=[];
+     for(let i = 0 ; i < size ; i++){
+        array[i]=[];
+     for(let j = 0 ; j < size ; j++){
+         array[i][j] = Number(prompt())
+     }
+  }
+  return array;
+}
+
+// Fun Adding Array Values
+function addingArrays(){
+    let arraySum = [];
+    for(let i = 0 ; i < size ; i++){
+        arraySum[i] = [];
+    for(let j = 0 ; j < size ; j++){
+        arraySum[i][j] = array1[i][j] + array2[i][j]
+    }
+  }
+  return arraySum;
+}
+
+//Printing Array 
+function DisplayArray(){
+  for(let i= 0 ; i < size ; i++){
+      console.log(array[i]+" ");
+  }
 }
