be1660a8893efdd678f3408d45cf23b2cd0b85d7
SIDHARTH T <sidhartht900@gmail.com>
2024-12-12 07:55:20 +0530

    Ass Q7

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
 // Accept input from the user
-let Num = Number(prompt("Enter a Number ( 1 to 7):")); 
+let Num = Number(prompt("Enter a Multiplication Table Number:")); 
 
-// Display Carespond Day
-console.log(Day());
+// Display Multiplecation Table 
+mTable();
 
-function Day(){
-    switch(Num){
-        case 1 :
-             result = "Sunday";
-             break;
-        case 2 :
-             result = "Monday";
-             break;
-        case 3 :
-             result = "Tuesday";
-             break;
-        case 4 :
-            result = "Wednesday";
-            break;
-        case 5 :
-             result = "Thursday";
-             break;
-        case 6 :
-             result = "Friday";
-             break;
-        case 7 :
-             result = "Saturday";
-             break;
-        default :
-             result = "Invalid Entry !!!"
+function mTable(){
+    for (let i = 1 ; i <=10 ; i ++){
+        console.log(i +" X "+Num+" = "+(i * Num) );
     }
-  return result;
 }
