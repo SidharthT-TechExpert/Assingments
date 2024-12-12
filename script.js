5c2771693eec7a9040b8872f7d60e8b45ea80afd
SIDHARTH T <sidhartht900@gmail.com>
2024-12-12 14:36:02 +0530

    Ass Q13

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-// get a Array size 
-let size = Number(prompt("Enter a array Size:"));
+// String Getting User
+let string = String(prompt("Enter A String: "));
+//Display Palindrome Or Not
+console.log(paliOrNot());
 
-// geting array value 
-const array = getArrayInput();
-
-console.log("Array is :"+array);
-
-//Sorting Of Array Desending Odear
-console.log("After Sorting Of Array Is: "+desSort());
-
-// Array Values Geting 
-function getArrayInput(){
-    let array = [];
-    
-    for(let i = 0 ;i < size ;i++){
-        let value = Number(prompt(`value ${i+1} : `));
-        array.push(value);
-    }
-  return array;
-}
-// Sorting Of Array Desending Odear
-function desSort(){
-    let temp=0;
-    for(let i = 0;i < size ;i++){
-        for(let j = i+1 ;j < size;j++){
-            if(array[i] < array[j]){
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
+// Function Checking whether a string is a palindrome or not
+function paliOrNot(){
+    size = string.length;
+    for(let i = 0;i < string.length;i++){
+        if((string[i])!=(string[size-i-1])){
+            result = "Entered string is not a palindrome";
+            break;
+        }else{
+            result = "Entered string is a palindrome";
         }
     }
-  return array;
+  return result;
 }
