3a53832c0527fed841939f063b05a670f3482341
SIDHARTH T <sidhartht900@gmail.com>
2024-12-13 10:32:54 +0530

    Ass Q 24

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function main() {
     // Declare Arrays
     let array = [];
-    let array2 = [];
-    
     // Array Size    
     let size = Number(prompt("Enter a array size: "));
     
     // Array Values Getting
-    console.log("Enter array 1 values:");
+    console.log("Enter array values:");
       array = getArray(size); // Get first array
-    console.log("Enter array 2 values:");
-      array2 = getArray(size); // Get second array
-     
-    // Sum of two arrays
-    let arrayA = sumArray(array, array2, size);
-    
     // Displaying Array
-    displayArray(arrayA);
+    displayArray(array);
 }
 
 // Function to get Array Values
@@ -39,18 +31,6 @@ function displayArray(array) {
     for (let i = 0; i < array.length; i++) {
         console.log(array[i].join(" ")); // Print each row of the array
     }
-}
-
-// Function to Sum Arrays
-function sumArray(array1, array2, size) {
-    let array = [];
-    for (let i = 0; i < size; i++) {
-        array[i] = [];
-        for (let j = 0; j < size; j++) {
-            array[i][j] = array1[i][j] + array2[i][j]; // Summing corresponding elements
-        }
-    }
-    return array;
 }
 
 main();
