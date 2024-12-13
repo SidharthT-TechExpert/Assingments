d0863c12cfe5de1823d1b74655712b2b12d7a5d0
SIDHARTH T <sidhartht900@gmail.com>
2024-12-13 06:05:13 +0530

    Ass Q 19

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-//Getting Anual income
-let Income = Number(prompt("Enter Your Anual Income : "));
 
-//Calling Tax 
-console.log("Income Tax Amount Is : "+tax());
- 
- 
- //Fun Tax 
- function tax(){
-     let L = 100000;
-     if (Income > 10 * L){
-         return (Income * 30) / 100;
-     }else if(Income > 5 * L){
-         return (Income * 20) / 100;
-     }else if(Income > 2.5 * L){
-         return (Income * 5) / 100;
-     }else{
-         return "No Tax"
-     }
- }
+let row = Number(prompt("Enter The Number Of Row : "));
+k = 1;
+for(let i = 1;i <= row ;i++){
+    for(let j = 1 ;j <= i; j++){
+        process.stdout.write((k++)+" ");
+    }
+  console.log()
+}
