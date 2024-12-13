33f6bdf6b4b8a6cc74d16a6da85814bba1114e50
SIDHARTH T <sidhartht900@gmail.com>
2024-12-13 13:13:28 +0530

    Ass Q 27

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-function validateHeight(userHeight) {
-    // Check if userHeight is NaN (Not-a-Number)
-    if (isNaN(userHeight)) {
-        throw new Error("not A Number Error");
-    }else if (userHeight > 200) {
-        throw new Error("HugeHeightError");
-    }else if (userHeight < 40) {
-        throw new Error("TinyHeightError");
-    }else{
-         console.log("valid");
-    }
+function Car(name, mileage, max_speed) {
+    this.name = name;
+    this.mileage = mileage;
+    this.max_speed = max_speed;
 }
 
-let userHeight = Number(prompt("Enter your valid Height :"));
-
-try {
-    validateHeight(userHeight);  
-} catch (error) {
-    console.log(error.message);  
-}
+// Example usage
+const myCar = new Car("Toyota", 30, 180);
+console.log(myCar.name);       
+console.log(myCar.mileage);    
+console.log(myCar.max_speed);  
