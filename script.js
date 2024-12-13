f5a9d466efd6439c1c97a31a67623152a3dfd21a
SIDHARTH T <sidhartht900@gmail.com>
2024-12-13 07:15:19 +0530

    Ass Q 21

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// getting size of array
+let size = Number(prompt("Enter a array limit: "));
 
-let row = Number(prompt("Enter The Number Of Row : "));
-k = 1;
-for(let i = 1;i <= row ;i++){
-    for(let j = 1 ;j <= i; j++){
-        process.stdout.write((k++)+" ");
+console.log(`Enter array ${size} values : `);
+// Getting Array Values in array
+let array = getArray();
+
+// Array Values Printing
+let array2 = mult();
+console.log("Output is: "+array2);
+//Fun getting array values
+function getArray(){
+    let array = [];
+     for(let i = 0; i < size ; i++){
+        let value = Number(prompt(`value ${i+1}: `));
+        array.push(value);
+     }
+  return array;
+}
+
+//Fun Mulit of adj value
+function mult(){
+    let marray = [];
+    for(let i = 0;i < size;i++){
+        if(i == size-1)break;
+       marray[i] = array[i] * array[i+1];
     }
-  console.log()
+  return marray;
 }
