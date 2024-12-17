96b8067623baf30c9df8ab7b0362b823f0032e23
SIDHARTH T <sidhartht900@gmail.com>
2024-12-17 17:09:53 +0530

    ASS Q 28

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function Car(name, mileage, max_speed) {
     this.max_speed = max_speed;
 }
 
+
 // Example usage
 const myCar = new Car("Toyota", 30, 180);
-console.log(myCar.name);       
+console.log(myCar.name);      
 console.log(myCar.mileage);    
-console.log(myCar.max_speed);  
+console.log(myCar.max_speed); 
